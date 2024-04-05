9c8abdacfec5951d1a0504c9859dcea5654f54f1
SimonSkywalker <sthordal@gmail.com>
2024-04-05 14:19:58 +0200

    Made some starting elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 
 export default function Home() {
   return (
-    <main>
       
-    </main>
+        <form className="max-w-sm mx-auto">
+          <h1>TimePlan</h1>
+          <div className="mb-5">
+            <label htmlFor="inp1" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+            <input type="text" id="inp1" className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="inp2" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+            <input type="text" id="inp2" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+          </div>
+          <label htmlFor="time" className="block mb-2 text-sm font-medium text-gray-900">Select time:</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+                    </svg>
+                </div>
+                <input type="time" id="time" className="border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " min="08:15" max="16:15" value="00:00" required />
+            </div>
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+        </form>
+
+    
   );
 }
